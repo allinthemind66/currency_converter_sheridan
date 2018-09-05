4125ca1fc7c14edb3de836abab0d92b824c14536
allinthemind66 <r.nilon92@gmail.com>
2018-09-05 00:06:16 -0400

    add script tag to html

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 // API CALL URL
 //NEED TO CHECK BACKEND CACHE BEFORE CALLING THIS API
 //`https://free.currencyconverterapi.com/api/v6/convert?q=${value1}_${value2}&compact=y`
+let submitButton = document.querySelector('button');
+let value1;
+let value2;
+submitButton.addEventListener('click', () => {
+  console.log("I'm converting!");
+});
