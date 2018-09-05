75ea1e6a36e1be81f91521f0eaac0c6140b33d9d
allinthemind66 <r.nilon92@gmail.com>
2018-09-05 14:49:08 -0400

    query all form elements

--- index.js
+++ index.js
@@ -2,8 +2,17 @@
 //NEED TO CHECK BACKEND CACHE BEFORE CALLING THIS API
 //`https://free.currencyconverterapi.com/api/v6/convert?q=${value1}_${value2}&compact=y`
 let submitButton = document.querySelector('button');
-let value1;
-let value2;
+// TODO: Get value when dom content loaded
+let value1Select = document.querySelector('.curr_select_one');
+let value2Select = document.querySelector('.curr_select_two');
+let value1 = value1Select.value;
+let value2 = value2Select.value;
+value1Select.addEventListener('change', () => {
+  console.log('value 1 is ', value1Select.value)
+});
+value2Select.addEventListener('change', () => {
+  console.log('value 2 is ', value2Select.value)
+});
 submitButton.addEventListener('click', () => {
   console.log("I'm converting!");
 });
